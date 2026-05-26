1034a68102583a795fb9fbb538df34c4438765f8
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-05-26 16:47:01 +0200

    feat: added nuxt icon module

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,7 +1,20 @@
+import { createResolver } from 'nuxt/kit'
+
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
 
   devtools: { enabled: true },
 
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
+
+  icon: {
+    customCollections: [
+      {
+        prefix: 'boulebaar',
+        dir: resolve('./app/assets/vectors'),
+      },
+    ],
+  },
 })
