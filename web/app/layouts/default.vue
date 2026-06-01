27b188abb02181bd264bd015a3faf7eb5ea88938
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-06-01 16:45:49 +0200

    style: padding between content and footer

--- web/app/layouts/default.vue
+++ web/app/layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen flex flex-col overflow-x-clip">
     <Navigation />
 
-    <main class="relative grow flex flex-col">
+    <main class="relative grow flex flex-col pb-20">
       <slot />
     </main>
 
