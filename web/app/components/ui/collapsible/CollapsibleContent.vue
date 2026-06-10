9f331caa40a43b2ba2e4508f305c0843bb75fc58
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-06-10 11:10:02 +0200

    feat: collapsible content animation

--- web/app/components/ui/collapsible/CollapsibleContent.vue
+++ web/app/components/ui/collapsible/CollapsibleContent.vue
@@ -6,7 +6,43 @@ const props = defineProps<CollapsibleContentProps>()
 </script>
 
 <template>
-  <CollapsibleContent data-slot="collapsible-content" v-bind="props">
+  <CollapsibleContent
+    data-slot="collapsible-content"
+    v-bind="props"
+    class="collapse-content"
+  >
     <slot />
   </CollapsibleContent>
 </template>
+
+<style scoped>
+.collapse-content {
+  overflow: hidden;
+}
+
+.collapse-content[data-state='open'] {
+  animation: slideDown 300ms ease-out;
+}
+
+.collapse-content[data-state='closed'] {
+  animation: slideUp 300ms ease-out;
+}
+
+@keyframes slideDown {
+  from {
+    height: 0;
+  }
+  to {
+    height: var(--reka-collapsible-content-height);
+  }
+}
+
+@keyframes slideUp {
+  from {
+    height: var(--reka-collapsible-content-height);
+  }
+  to {
+    height: 0;
+  }
+}
+</style>
